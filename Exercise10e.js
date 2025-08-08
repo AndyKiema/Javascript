d4e4e07e6ad72d07d9a03f27f2f7ffe75ee2e371
Andrew Kiema <andrewkiema94@gmail.com>
2025-08-08 12:29:59 +0300

    Allow only one button to be toggled

--- Exercise10e.js
+++ Exercise10e.js
@@ -1,29 +1,50 @@
+let gamingButton;
 function toggleGamingButton(){
-  const gamingButton=document.querySelector('.gaming-button');
-
-  if(gamingButton.classList.contains('isToggled')){
-    gamingButton.classList.remove('isToggled');
-  }else{
-    gamingButton.classList.add('isToggled');
-  }
+   gamingButton=document.querySelector('.gaming-button');
+   if(gamingButton.classList.contains('isToggled')){
+      gamingButton.classList.remove('isToggled');
+   }else{
+      gamingButton.classList.add('isToggled');
+   } 
 }
 
+let musicButton;
 function toggleMusicButton(){
-  const musicButton=document.querySelector('.music-button');
-
-  if(musicButton.classList.contains('isToggled')){
-    musicButton.classList.remove('isToggled');
-  }else{
+   musicButton=document.querySelector('.music-button');
+   if(musicButton.classList.contains('isToggled')){
+      musicButton.classList.remove('isToggled');
+   }else{
     musicButton.classList.add('isToggled');
   }
 }
-  
-function toggleTechButton(){
-  const techButton=document.querySelector('.tech-button');
 
+let techButton;
+function toggleTechButton(){
+  techButton=document.querySelector('.tech-button');
   if(techButton.classList.contains('isToggled')){
     techButton.classList.remove('isToggled');
   }else{
     techButton.classList.add('isToggled');
-  }
+  } 
 }   
+
+function disableGamingTech(){
+  if(gamingButton.classList.contains('isToggled')||techButton.classList.contains('isToggled')){
+    gamingButton.classList.remove('isToggled');
+    techButton.classList.remove('isToggled')
+  }
+}
+
+function disableMusicTech(){
+  if(musicButton.classList.contains('isToggled')||techButton.classList.contains('isToggled')){
+    musicButton.classList.remove('isToggled');
+    techButton.classList.remove('isToggled')
+  }
+}
+
+function disableGamingMusic(){
+  if(gamingButton.classList.contains('isToggled')||musicButton.classList.contains('isToggled')){
+    gamingButton.classList.remove('isToggled');
+    musicButton.classList.remove('isToggled')
+  }
+}
