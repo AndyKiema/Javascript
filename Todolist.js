4cb9d40ab31f652025364a2321cd3f8c364c0138
Andrew Kiema <andrewkiema94@gmail.com>
2025-08-18 11:38:36 +0300

    Add delete button

--- Todolist.js
+++ Todolist.js
@@ -9,7 +9,13 @@ function addToHTML(){
   let htmlList='';
   for(let i=0;i<todoList.length;i++){
    let toDo=todoList[i];
-   let HTML=`<p>${toDo}</p>`
+   let HTML=`<p>
+   ${toDo}
+   <button onclick="
+    todoList.splice(${i},1);
+    addToHTML();
+   ">Delete</button>
+   </p>`
    htmlList+=HTML;
    const textDiv=document.querySelector(".todo-container");
    textDiv.innerHTML=htmlList;
